55833fd21c3ae12ff785e8f7323fa02e4c554563
rwilli84 <js4zygxv5h@privaterelay.appleid.com>
2026-06-13 17:24:48 -0600

    update to vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/X-COM-2-Companion/',
   plugins: [
     react(),
     tailwindcss(),
