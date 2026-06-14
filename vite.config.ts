3d6937b3092a159ba29d36294096e9d958ca0c9a
rwilli84 <js4zygxv5h@privaterelay.appleid.com>
2026-06-14 16:49:47 -0600

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/X-COM-2-Companion/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
